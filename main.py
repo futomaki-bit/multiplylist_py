79407fee5a13aa2835303bd1736d79d7e3505896
futomakiyoin <futomakiyoin@gmail.com>
2021-06-06 21:53:04 -0400

    Initial Upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+# m is a multiplier, chose 1.25 which works well with float
+m = 1.25
+# create random list of range 1 to 1000, with 5 items
+firstList = [float(i) for i in random.sample(range(1, 1000), 5)]
+randomList = firstList[:]
+print("initial list", randomList)
+
+# This one does not affect the list
+for item in randomList:
+    item *= m
+print("first try", randomList)
+
+# This one does
+randomList = firstList[:]
+for index in range(len(randomList)):
+    randomList[index] *= m
+print("second try", randomList)
+
+# This also does
+randomList = firstList[:]
+randomList = [i*m for i in randomList]
+print("third try", randomList)
